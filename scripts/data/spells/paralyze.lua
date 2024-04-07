7ac170489af89c8f0aa6bc9eec9e235fa1a8c0b6
Nelle <157313460+NelleMonelle@users.noreply.github.com>
2024-04-07 18:30:01 +0300

    Paralyze updated

--- scripts/data/spells/paralyze.lua
+++ scripts/data/spells/paralyze.lua
@@ -36,9 +36,34 @@ function spell:onCast(user, target)
 	Game.battle.timer:after(15/30, function()
 		target:flash()
 		target:hurt(damage, user)
+		Game.battle:finishActionBy(user)
 	end)
 
 	generateSlash(1)
+	return false
+end
+
+function spell:onLightCast(user, target)
+	local damage = math.floor((((user.chara:getStat("magic") * 150) / 20) - 3 * (target.defense)) * 1.3)
+
+	local function generateSlash(scale_x)
+		local cutAnim = Sprite("effects/attack/diamond")
+		cutAnim:setOrigin(0.5, 0.5)
+		cutAnim:setScale(3.5 * scale_x, 3.5)
+		cutAnim:setPosition(target:getRelativePos(target.width/2, target.height/2))
+		cutAnim.layer = target.layer + 0.01
+		Assets.playSound("trap")
+		cutAnim:play(1/20, false, function(s) s:remove() end)
+		user.parent:addChild(cutAnim)
+	end
+
+	Game.battle.timer:after(15/30, function()
+		target:hurt(damage, user)
+		Game.battle:finishActionBy(user)
+	end)
+
+	generateSlash(1)
+	return false
 end
 
 return spell
